6042b79ec7335504f3e5114f8af7720603fa6f1e
Hannes Struss <x@hannesstruss.de>
2016-05-13 00:46:51 +0200

    Implement top half bonus

--- core/src/main/kotlin/chonx/Game.kt
+++ core/src/main/kotlin/chonx/Game.kt
@@ -1,11 +1,15 @@
 package chonx
 
+import chonx.Slot.*
+
 class Game private constructor(val players: List<Player>,
                                val currentPlayer: Player,
                                private val moves: List<Move>) {
 
   companion object {
     fun new(players: List<Player>) = Game(players, players[0], listOf())
+
+    private val TOP_HALF_SLOTS = setOf(ACES, TWOS, THREES, FOURS, FIVES, SIXES)
   }
 
   fun move(roll: DiceRoll, slot: Slot): Game {
@@ -15,8 +19,17 @@ class Game private constructor(val players: List<Player>,
     return Game(players, nextPlayer, moves + move)
   }
 
-  fun points(player: Player): Int = moves
-      .filter { it.player == player }
-      .map { it.slot.points(it.diceRoll) }
-      .sum()
+  fun score(player: Player): Int {
+    val playerMoves = moves.filter { it.player == player }
+        .partition { it.slot in TOP_HALF_SLOTS }
+
+    val topHalfScore = playerMoves.first
+        .map { it.slot.points(it.diceRoll) }
+        .sum()
+        .let { if (it > 63) it + 35 else it }
+
+    val bottomHalfScore = playerMoves.second.map { it.slot.points(it.diceRoll) }.sum()
+
+    return topHalfScore + bottomHalfScore
+  }
 }
--- core/src/test/kotlin/chonx/GameTest.kt
+++ core/src/test/kotlin/chonx/GameTest.kt
@@ -27,6 +27,19 @@ class GameTest {
 
     val nextGame = game.move(DiceRoll(1, 1, 1, 1, 1), Slot.CHANCE)
 
-    assertThat(nextGame.points(hannes)).isEqualTo(5)
+    assertThat(nextGame.score(hannes)).isEqualTo(5)
+  }
+
+  @Test fun shouldCalculateUpperHalfBonus() {
+    val score = Game.new(listOf(hannes))
+        .move(DiceRoll(1, 1, 1, 1, 1), Slot.ACES)
+        .move(DiceRoll(2, 2, 2, 2, 2), Slot.TWOS)
+        .move(DiceRoll(3, 3, 3, 3, 3), Slot.THREES)
+        .move(DiceRoll(4, 4, 4, 4, 4), Slot.FOURS)
+        .move(DiceRoll(5, 5, 5, 5, 5), Slot.FIVES)
+        .move(DiceRoll(6, 6, 6, 6, 6), Slot.SIXES)
+        .score(hannes)
+
+    assertThat(score).isEqualTo(140)
   }
 }
