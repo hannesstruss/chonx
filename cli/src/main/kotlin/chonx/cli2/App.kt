49fbb6b6a47ca819480a7d847e1623198c250102
Hannes Struß <x@hannesstruss.de>
2016-05-21 19:05:12 +0200

    Fix warning

--- cli/src/main/kotlin/chonx/cli2/App.kt
+++ cli/src/main/kotlin/chonx/cli2/App.kt
@@ -1,9 +1,9 @@
 package chonx.cli2
 
 fun main(args: Array<String>) {
-  var state = StateMachine.new()
+  val state = StateMachine.new()
 
-  state = state
+  state
       .handle(Command.AddPlayer("Hannes"))
       .handle(Command.AddPlayer("Dieter"))
       .handle(Command.BeginGame())
