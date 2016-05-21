6acbd9e124892c7dc9e4eacf9077260557e85ab9
Hannes Struß <x@hannesstruss.de>
2016-05-21 20:13:57 +0200

    CLI that works

---
+++ cli/src/main/kotlin/chonx/cli/App.kt
@@ -0,0 +1,49 @@
+package chonx.cli
+
+import chonx.statemachine.Command
+import chonx.statemachine.Phase
+import chonx.statemachine.StateMachine
+
+fun main(args: Array<String>) {
+  var state = StateMachine.new()
+
+  state = state
+      .handle(Command.AddPlayer("Hannes"))
+      .handle(Command.AddPlayer("Dieter"))
+      .handle(Command.BeginGame())
+
+  while (true) {
+    println("")
+    val phase = state.phase
+    if (phase is Phase.GamePhase) {
+      println("${phase.javaClass.simpleName} - Current: ${phase.game.currentPlayer.name} (${phase.game.score(phase.game.currentPlayer)})")
+
+      if (phase is Phase.MovePhase) {
+        val msg = phase.moveInProgress.dice()
+            .mapIndexed { index, die ->
+              if (phase.moveInProgress.isLocked(index)) {
+                "(${die})"
+              } else {
+                die.toString()
+              }
+            }.joinToString(" ")
+        println(msg)
+      }
+    }
+    print("> ")
+    val input = readLine()
+
+    if (input == null) {
+      break
+    }
+
+    try {
+      val cmd = parse(input.trim())
+      state = state.handle(cmd)
+    } catch (e: ParseException) {
+      println(e.message)
+      continue
+    }
+  }
+}
+
---
+++ cli/src/main/kotlin/chonx/cli/parse.kt
@@ -0,0 +1,37 @@
+package chonx.cli
+
+import chonx.core.Slot
+import chonx.statemachine.Command
+
+val LockRegex = """^lock (\d(, \d)*)""".toRegex()
+val UnlockRegex = """^unlock (\d(, \d)*)""".toRegex()
+val PickRegex = """pick (\w+)""".toRegex()
+
+fun parse(cmd: String): Command {
+  LockRegex.find(cmd)?.let {
+    val indices = it.groupValues[1].split(",").map { it.trim().toInt() - 1 }
+    return Command.LockDice(indices)
+  }
+
+  UnlockRegex.find(cmd)?.let {
+    val indices = it.groupValues[1].split(",").map { it.trim().toInt() - 1 }
+    return Command.UnlockDice(indices)
+  }
+
+  PickRegex.find(cmd)?.let {
+    val slotId = it.groupValues[1].trim()
+    val slot = Slot.byId(slotId)
+    if (slot == null) {
+      throw ParseException("Can't parse slot ${slotId}")
+    }
+    return Command.PickSlot(slot)
+  }
+
+  return when (cmd) {
+    "roll" -> Command.RollDice()
+    "accept" -> Command.AcceptDice()
+    else -> throw ParseException("Can't parse command ${cmd}")
+  }
+}
+
+class ParseException(message: String) : IllegalArgumentException(message)
