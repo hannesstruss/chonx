f3dd519c2321dd3bd2a40fd16f60aeb82820879e
Hannes Struß <x@hannesstruss.de>
2016-05-28 16:11:27 +0200

    Decouple input from state

--- cli/src/main/kotlin/chonx/cli/App.kt
+++ cli/src/main/kotlin/chonx/cli/App.kt
@@ -1,20 +1,78 @@
 package chonx.cli
 
 import chonx.core.IllegalMoveException
+import chonx.core.Player
 import chonx.core.SlotAlreadyFilledException
+import chonx.statemachine.Command
 import chonx.statemachine.Phase
 import chonx.statemachine.StateMachine
+import rx.Observable
+import rx.subjects.PublishSubject
 
 fun main(args: Array<String>) {
-  var state = StateMachine.new()
+  val inputAdapter = CliInputAdapter()
+  val gameLoop = GameLoop(inputAdapter)
+  gameLoop.start()
+  inputAdapter.start()
+}
 
-  while (true) {
-    println("")
+interface InputAdapter {
+  fun commands(): Observable<Pair<Player, Command>>
+  fun send(player: Player, msg: String)
+}
+
+class CliInputAdapter : InputAdapter {
+  private val commands: PublishSubject<Pair<Player, Command>> = PublishSubject.create()
+
+  override fun commands() = commands
+
+  override fun send(player: Player, msg: String) {
+    println("@${player.name}: ${msg}")
+  }
+
+  fun start() {
+    while (true) {
+      print("> ")
+      val input = readLine()
+
+      if (input == null) {
+        break
+      }
+
+      try {
+        val cmd = parse(input.trim())
+        commands.onNext(Player("Hannes") to cmd)
+      } catch (e: ParseException) {
+        println(e.message)
+        continue
+      }
+    }
+  }
+}
+
+class GameLoop(val adapter: InputAdapter) {
+  private var state = StateMachine.new()
+
+  fun start() {
+    status()
+    adapter.commands().subscribe {
+      try {
+        state = state.handle(it.second)
+      } catch (e: IllegalMoveException) {
+        println(e.message)
+      } catch (e: SlotAlreadyFilledException) {
+        println(e.message)
+      }
+      status()
+    }
+  }
+
+  private fun status() {
     val phase = state.phase
     if (phase is Phase.CollectPlayers) {
-      println("""Players: ${phase.preGame.players.map { it.name }.joinToString(", ")}""")
+      adapter.send(Player("Hannes"), """Players: ${phase.preGame.players.map { it.name }.joinToString(", ")}""")
     } else if (phase is Phase.GamePhase) {
-      println("${phase.javaClass.simpleName} - Current: ${phase.game.currentPlayer.name} (${phase.game.score(phase.game.currentPlayer)})")
+      adapter.send(Player("Hannes"), "${phase.javaClass.simpleName} - Current: ${phase.game.currentPlayer.name} (${phase.game.score(phase.game.currentPlayer)})")
 
       if (phase is Phase.MovePhase) {
         val msg = phase.moveInProgress.dice()
@@ -25,32 +83,11 @@ fun main(args: Array<String>) {
                 die.toString()
               }
             }.joinToString(" ")
-        println(msg)
+        adapter.send(Player("Hannes"), msg)
       }
     } else if (phase is Phase.Ended) {
-      println("Done!")
+      adapter.send(Player("Hannes"), "Done!")
       System.exit(0)
-    }
-
-    print("> ")
-    val input = readLine()
-
-    if (input == null) {
-      break
-    }
-
-    try {
-      val cmd = parse(phase, input.trim())
-      state = state.handle(cmd)
-    } catch (e: ParseException) {
-      println(e.message)
-      continue
-    } catch (e: IllegalMoveException) {
-      println(e.message)
-      continue
-    } catch (e: SlotAlreadyFilledException) {
-      println(e.message)
-      continue
     }
   }
 }
--- cli/src/main/kotlin/chonx/cli/parse.kt
+++ cli/src/main/kotlin/chonx/cli/parse.kt
@@ -2,47 +2,42 @@ package chonx.cli
 
 import chonx.core.Slot
 import chonx.statemachine.Command
-import chonx.statemachine.Phase
 
+val AddPlayerRegex = """add (\w+)""".toRegex()
 val LockRegex = """^lock (\d(, \d)*)""".toRegex()
 val UnlockRegex = """^unlock (\d(, \d)*)""".toRegex()
 val PickRegex = """pick (\w+)""".toRegex()
 
-fun parse(phase: Phase, cmd: String): Command {
-  if (phase is Phase.CollectPlayers) {
-    if (cmd == "begin") {
-      return Command.BeginGame()
-    } else {
-      return Command.AddPlayer(cmd)
-    }
-  } else if (phase is Phase.GamePhase) {
-    LockRegex.find(cmd)?.let {
-      val indices = it.groupValues[1].split(",").map { it.trim().toInt() - 1 }
-      return Command.LockDice(indices)
-    }
-
-    UnlockRegex.find(cmd)?.let {
-      val indices = it.groupValues[1].split(",").map { it.trim().toInt() - 1 }
-      return Command.UnlockDice(indices)
-    }
-
-    PickRegex.find(cmd)?.let {
-      val slotId = it.groupValues[1].trim()
-      val slot = Slot.byId(slotId)
-      if (slot == null) {
-        throw ParseException("Can't parse slot ${slotId}")
-      }
-      return Command.PickSlot(slot)
-    }
-
-    return when (cmd) {
-      "roll" -> Command.RollDice()
-      "accept" -> Command.AcceptDice()
-      else -> throw ParseException("Can't parse command ${cmd}")
-    }
+fun parse(cmd: String): Command {
+  AddPlayerRegex.find(cmd)?.let {
+    return Command.AddPlayer(it.groupValues[1])
   }
 
-  throw IllegalStateException("Can't parse command in phase: ${phase.javaClass}")
+  LockRegex.find(cmd)?.let {
+    val indices = it.groupValues[1].split(",").map { it.trim().toInt() - 1 }
+    return Command.LockDice(indices)
+  }
+
+  UnlockRegex.find(cmd)?.let {
+    val indices = it.groupValues[1].split(",").map { it.trim().toInt() - 1 }
+    return Command.UnlockDice(indices)
+  }
+
+  PickRegex.find(cmd)?.let {
+    val slotId = it.groupValues[1].trim()
+    val slot = Slot.byId(slotId)
+    if (slot == null) {
+      throw ParseException("Can't parse slot ${slotId}")
+    }
+    return Command.PickSlot(slot)
+  }
+
+  return when (cmd) {
+    "begin" -> Command.BeginGame()
+    "roll" -> Command.RollDice()
+    "accept" -> Command.AcceptDice()
+    else -> throw ParseException("Can't parse command ${cmd}")
+  }
 }
 
 class ParseException(message: String) : IllegalArgumentException(message)
